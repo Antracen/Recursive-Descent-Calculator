7b38efe5723fb40ce374b01cb908ef85c5bc47fe
Martin Wass <mawass@kth.se>
2017-06-27 14:28:03 +0200

    Handles multiplication and division

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ def main():
 def tokenize(user_input):
 	input_position = 0
 	tokens = []
-	token_pattern = re.compile("\d+|\n|\+|\-")
+	token_pattern = re.compile("\d+|\n|\+|\-|\/|\*")
 	token_matches = token_pattern.finditer(user_input)
 	
 	for match in token_matches:
@@ -26,6 +26,12 @@ def tokenize(user_input):
 			input_position += len(match.group())
 		if re.compile("\-").match(match.group()):
 			tokens.append(["MINUS"])
+			input_position += len(match.group())
+		if re.compile("\*").match(match.group()):
+			tokens.append(["MULTIPLY"])
+			input_position += len(match.group())
+		if re.compile("\/").match(match.group()):
+			tokens.append(["DIVIDE"])
 			input_position += len(match.group())
 		if re.compile("\n").match(match.group()):
 			tokens.append(match.group())
@@ -42,21 +48,21 @@ def parse(tokens):
 def parse_calculation(tokens):
 	parsetree = []
 	tokens_parsed = 0
-	result, tokens = parse_number(tokens)
+	result, tokens = parse_term(tokens)
 	while len(tokens) > 0:
 		next = tokens.pop(0)
 		if next[0] == "PLUS":
-			plus, tokens = parse_number(tokens)
+			plus, tokens = parse_term(tokens)
 			result += plus
 		elif next[0] == "MINUS":
-			minus, tokens = parse_number(tokens)
+			minus, tokens = parse_term(tokens)
 			result -= minus
 		else:
 			result = "SYNTAX ERROR, EXPECTED PLUS OR MINUS, GOT \"" + next[0] + "\""
 			break
 	return result
 
-def parse_number(tokens):
+def parse_term(tokens):
 	if len(tokens) == 0:
 		print("SYNTAX ERROR: EXPECTED NUMBER, GOT NULL")
 		sys.exit()
@@ -64,7 +70,19 @@ def parse_number(tokens):
 		print("SYNTAX ERROR: EXPECTED NUMBER, GOT " + tokens[0][0])
 		sys.exit()
 	else:
-		return int(tokens.pop(0)[1]), tokens
+		result = int(tokens.pop(0)[1])
+		while len(tokens) > 0:
+			if tokens[0][0] == "MULTIPLY":
+				tokens.pop(0)
+				multiply, tokens = parse_term(tokens)
+				result *= multiply
+			elif tokens[0][0] == "DIVIDE":
+				tokens.pop(0)
+				divide, tokens = parse_term(tokens)
+				result /= divide
+			else:
+				break
+	return result, tokens
 
 if __name__ == "__main__":
 	main()
