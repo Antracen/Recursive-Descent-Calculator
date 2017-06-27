7dc3720cec3ad294eec2ab04cfba29d3630aaab3
Martin Wass <mawass@kth.se>
2017-06-27 15:12:02 +0200

    Calculator handles parentheses. Needs better documentation

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ def main():
 def tokenize(user_input):
 	input_position = 0
 	tokens = []
-	token_pattern = re.compile("\d+|\n|\+|\-|\/|\*")
+	token_pattern = re.compile("\d+|\n|\+|\-|\/|\*|\(|\)")
 	token_matches = token_pattern.finditer(user_input)
 	
 	for match in token_matches:
@@ -33,6 +33,12 @@ def tokenize(user_input):
 		if re.compile("\/").match(match.group()):
 			tokens.append(["DIVIDE"])
 			input_position += len(match.group())
+		if re.compile("\(").match(match.group()):
+			tokens.append(["LEFTPAREN"])
+			input_position += len(match.group())
+		if re.compile("\)").match(match.group()):
+			tokens.append(["RIGHTPAREN"])
+			input_position += len(match.group())
 		if re.compile("\n").match(match.group()):
 			tokens.append(match.group())
 			input_position += len(match.group())
@@ -42,7 +48,9 @@ def tokenize(user_input):
 
 # Make parse tree.
 def parse(tokens):
-	result = parse_calculation(tokens);
+	result, tokens = parse_calculation(tokens);
+	if len(tokens) != 0:
+		return "SYNTAX ERROR. Remaining invalid character at " + str(tokens[0][2]) + ": \"" + tokens[0][1] + "\""
 	return result
 
 def parse_calculation(tokens):
@@ -50,38 +58,48 @@ def parse_calculation(tokens):
 	tokens_parsed = 0
 	result, tokens = parse_term(tokens)
 	while len(tokens) > 0:
-		next = tokens.pop(0)
-		if next[0] == "PLUS":
+		if tokens[0][0] == "PLUS":
+			tokens.pop(0)
 			plus, tokens = parse_term(tokens)
 			result += plus
-		elif next[0] == "MINUS":
+		elif tokens[0][0] == "MINUS":
+			tokens.pop(0)
 			minus, tokens = parse_term(tokens)
 			result -= minus
 		else:
-			result = "SYNTAX ERROR, EXPECTED PLUS OR MINUS, GOT \"" + next[0] + "\""
 			break
-	return result
+	return result, tokens
 
 def parse_term(tokens):
+	result, tokens = parse_factor(tokens)
+	while len(tokens) > 0:
+		if tokens[0][0] == "MULTIPLY":
+			tokens.pop(0)
+			multiply, tokens = parse_factor(tokens)
+			result *= multiply
+		elif tokens[0][0] == "DIVIDE":
+			tokens.pop(0)
+			divide, tokens = parse_factor(tokens)
+			result /= divide
+		else:
+			break
+	return result, tokens
+
+def parse_factor(tokens):
 	if len(tokens) == 0:
-		print("SYNTAX ERROR: EXPECTED NUMBER, GOT NULL")
+		print("SYNTAX ERROR: EXPECTED FACTOR, GOT NULL")
 		sys.exit()
-	elif tokens[0][0] != "NUMBER":
-		print("SYNTAX ERROR: EXPECTED NUMBER, GOT " + tokens[0][0])
-		sys.exit()
-	else:
+	elif tokens[0][0] == "NUMBER":
 		result = int(tokens.pop(0)[1])
-		while len(tokens) > 0:
-			if tokens[0][0] == "MULTIPLY":
-				tokens.pop(0)
-				multiply, tokens = parse_term(tokens)
-				result *= multiply
-			elif tokens[0][0] == "DIVIDE":
-				tokens.pop(0)
-				divide, tokens = parse_term(tokens)
-				result /= divide
-			else:
-				break
+	elif tokens[0][0] == "LEFTPAREN":
+		tokens.pop(0)
+		result, tokens = parse_calculation(tokens)
+		if not tokens[0][0] == "RIGHTPAREN":
+			print("SYNTAX ERROR: EXPECTED RIGHT PARENTHESES, GOT " + tokens[0][0])
+		else:
+			tokens.pop(0)
+	else:
+		print("SYNTAX ERROR: EXPECTED FACTOR, GOT: " + tokens[0][0])
 	return result, tokens
 
 if __name__ == "__main__":
